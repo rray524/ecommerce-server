307fb53dc225505ce940565f3bb98d2ba7b680fd
rray524 <rray524@gmail.com>
2021-12-29 20:17:55 +0600

    DB connection established

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
+mongoose
+    .connect(
+        "mongodb+srv://up_bihar_sotsongo:A84evSqopn4AHpej@cluster0.4jhnt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    )
+    .then(() => console.log('DB Connected successfully'))
+    .catch((err) => console.log(err))
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("Backend server is running!");
